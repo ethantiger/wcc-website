6b9895747adef8abf09a62c0749297d0bb83bc92
Ethan Tiger Wakefield <92559675+ethantiger@users.noreply.github.com>
2025-07-02 22:48:54 -0400

    Switch to ts (#2)

---
+++ src/hooks/useLogin.ts
@@ -0,0 +1,34 @@
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
+import { auth } from "../firebase/config";
+import { useAuthContext } from "./useAuthContext";
+
+interface UseLoginResult {
+  login: (email: string, password: string) => Promise<void>;
+  error: string | null;
+  isPending: boolean;
+}
+
+export const useLogin = (): UseLoginResult => {
+  const [error, setError] = useState<string | null>(null);
+  const [isPending, setIsPending] = useState<boolean>(false);
+  const { dispatch } = useAuthContext();
+
+  const login = async (email: string, password: string): Promise<void> => {
+    setError(null);
+    setIsPending(true);
+
+    try {
+      const res = await signInWithEmailAndPassword(auth, email, password);
+      dispatch({ type: "LOGIN", payload: res.user });
+
+      setIsPending(false);
+      setError(null);
+    } catch (err: any) {
+      setError(err.message);
+      setIsPending(false);
+    }
+  };
+
+  return { login, error, isPending };
+};
