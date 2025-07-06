801a5c6580528efcf847ae13c91b4ac265f583fa
Ethan Tiger Wakefield <ethanwakefield007@gmail.com>
2025-07-06 15:26:49 -0400

    remove

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import ParallaxMountains from "./components/ParallaxMountain"
 import Login from "./pages/Login";
