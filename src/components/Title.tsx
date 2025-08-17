d69332c4134bcc652e7da240c4c3bec02afe5c4f
Ethan Tiger Wakefield <ethanwakefield007@gmail.com>
2025-08-17 12:05:08 -0400

    cool button

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -111,11 +111,11 @@ export default function Title() {
           transition={{ duration: 0.3, delay: 1 }}
           className="relative top-4/12 md:top-[50vh] translate-y-[-50%] z-10 mt-8 flex flex-wrap items-center justify-center gap-4"
         >
-          <a
-          href="https://westernusc.store/product/western-climbing-club/"
-          className="text-center w-60 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 dark:bg-white dark:text-black dark:hover:bg-gray-200"
-          >
-          Become a Member!
+          <a target="_blank" href="https://westernusc.store/product/western-climbing-club/" className="relative inline-flex h-12 overflow-hidden rounded-full p-[4px] transition-all duration-300 hover:-translate-y-0.5 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+            <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+            <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-white hover:bg-gray-200 px-3 py-1 text-sm font-medium text-black backdrop-blur-3xl">
+              Become a Member!
+            </span>
           </a>
         </motion.div>
       </ParallaxLayer>
