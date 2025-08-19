044b6b25d385eff613b26ca9088a7806419c4404
Ethan Tiger Wakefield <ethanwakefield007@gmail.com>
2025-08-18 23:42:00 -0400

    add some dummy data

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -1,8 +1,8 @@
 import {Carousel, Card} from "./ui/apple-cards-carousel";
 
 export default function Events() {
-  const cards = data.map((card, index) => (
-    <Card key={card.src} card={card} index={index} />
+  const cards = data.map((card) => (
+    <Card key={card.src} card={card} />
   ));
 
   return (
@@ -49,40 +49,21 @@ const DummyContent = () => {
  
 const data = [
   {
-    category: "Artificial Intelligence",
-    title: "You can do more with AI.",
-    src: "https://images.unsplash.com/photo-1593508512255-86ab42a8e620?q=80&w=3556&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    category: "Upcoming",
+    title: "Niagara Glen Climbing Trip",
+    src: "https://niagaraglenclimbing.ca/images/gallery/170429-zack_on_sabishii.webp",
     content: <DummyContent />,
   },
   {
-    category: "Productivity",
-    title: "Enhance your productivity.",
-    src: "https://images.unsplash.com/photo-1531554694128-c4c6665f59c2?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    category: "August 17, 2025",
+    title: "Junction Climb Nights",
+    src: "https://images.squarespace-cdn.com/content/v1/61df2c7d4f6f0b3f0cb28707/433f31d8-f715-4a56-acbb-c5caf66a0a42/junctionEvanExportsEZ403930.jpg",
     content: <DummyContent />,
   },
   {
-    category: "Product",
-    title: "Launching the new Apple Vision Pro.",
-    src: "https://images.unsplash.com/photo-1713869791518-a770879e60dc?q=80&w=2333&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    category: "Upcoming",
+    title: "Guelph Grotto Competition",
+    src: "https://static.wixstatic.com/media/98f2ba_9df96cb1a6ff4ca688c124eeda4b2768~mv2.png/v1/fill/w_970,h_644,al_c,q_90,enc_avif,quality_auto/bouldering%20img.png",
     content: <DummyContent />,
-  },
- 
-  {
-    category: "Product",
-    title: "Maps for your iPhone 15 Pro Max.",
-    src: "https://images.unsplash.com/photo-1599202860130-f600f4948364?q=80&w=2515&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    content: <DummyContent />,
-  },
-  {
-    category: "iOS",
-    title: "Photography just got better.",
-    src: "https://images.unsplash.com/photo-1602081957921-9137a5d6eaee?q=80&w=2793&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    content: <DummyContent />,
-  },
-  {
-    category: "Hiring",
-    title: "Hiring for a Staff Software Engineer",
-    src: "https://images.unsplash.com/photo-1511984804822-e16ba72f5848?q=80&w=2048&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    content: <DummyContent />,
-  },
+  }
 ];
