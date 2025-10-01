6e4ef996833921fe2c8e655c4b0c4b8140f41a89
Ethan Tiger Wakefield <ethanwakefield007@gmail.com>
2025-10-01 16:40:14 -0400

    add wcc logo

--- src/features/dashboard/components/Sidebar.tsx
+++ src/features/dashboard/components/Sidebar.tsx
@@ -9,6 +9,8 @@ import {
 } from "@tabler/icons-react";
 import { motion } from "motion/react";
 
+import WCCLogo from "@/assets/WCC-logo-symbol.png";
+
 
 import { useLogout } from "@/hooks/useLogout";
 
@@ -44,7 +46,7 @@ const Logo = () => {
       onClick={() => navigate("/")}
       className="relative z-20 flex items-center space-x-2 py-1 text-sm font-normal text-black hover:cursor-pointer"
     >
-      <div className="h-5 w-6 shrink-0 rounded-tl-lg rounded-tr-sm rounded-br-lg rounded-bl-sm bg-black dark:bg-white" />
+      <img src={WCCLogo} alt="WCC Logo" className="h-5" />
       <motion.span
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
@@ -61,7 +63,7 @@ const LogoIcon = () => {
     <a
       className="relative z-20 flex items-center space-x-2 py-1 text-sm font-normal text-black hover:cursor-pointer"
     >
-      <div className="h-5 w-6 shrink-0 rounded-tl-lg rounded-tr-sm rounded-br-lg rounded-bl-sm bg-black dark:bg-white" />
+      <img src={WCCLogo} alt="WCC Logo" className="h-5 shrink-0" />
     </a>
   );
 };
