a77371bc49cb47f84e5ccc92995a1115967e1d20
Ethan Tiger Wakefield <ethanwakefield007@gmail.com>
2025-12-23 00:23:02 -0500

    add squiggles

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -72,8 +72,27 @@ export default function About() {
   ];
 
   return (
-    <section id="team" className="min-h-screen bg-[#10091e] py-12 md:py-0">
-      <div className="flex flex-col md:flex-row items-center justify-start text-gray-200">
+    <section id="team" className="min-h-screen relative overflow-hidden py-12 md:py-0">
+      {/* Gradient Background */}
+      <div className="absolute inset-0 bg-[#0f0c29]"></div>
+      
+      {/* Top Squiggly Border */}
+      <div className="absolute top-0 left-0 w-full overflow-hidden leading-[0]">
+        <svg className="relative block w-full h-[60px] md:h-[100px]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+          <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" 
+                className="fill-[#10091e]"></path>
+        </svg>
+      </div>
+
+      {/* Bottom Squiggly Border */}
+      <div className="absolute bottom-0 left-0 w-full overflow-hidden leading-[0] rotate-180">
+        <svg className="relative block w-full h-[60px] md:h-[100px]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+          <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" 
+                className="fill-[#10091e]"></path>
+        </svg>
+      </div>
+
+      <div className="flex flex-col md:flex-row items-center justify-start text-gray-200 relative z-10 pt-36">
         
         {/* Rotated Title */}
         <div className="flex justify-center items-center md:h-screen h-[200px] md:w-[250px] w-full md:mr-8">
